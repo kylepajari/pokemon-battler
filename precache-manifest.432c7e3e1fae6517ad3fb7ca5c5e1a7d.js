0bdedbab262b5f343c2c64bad62fc0cc79580e30
Kyle Pajari <kylepajari@yahoo.com>
2019-01-31 11:21:56 -0600

    Updates

---
+++ precache-manifest.432c7e3e1fae6517ad3fb7ca5c5e1a7d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "81c0aae9bb2eb54bf8fe07e1b68d829e",
+    "url": "/pokedex-battler/static/media/PressStart2P-Regular.81c0aae9.ttf"
+  },
+  {
+    "revision": "bdda37c9331b1d4ef380",
+    "url": "/pokedex-battler/static/css/main.4b3d9e98.chunk.css"
+  },
+  {
+    "revision": "de20ffb61759ca92878b995bd1ed6eb9",
+    "url": "/pokedex-battler/static/media/rockicon.de20ffb6.png"
+  },
+  {
+    "revision": "0b66087019812d1c3da3",
+    "url": "/pokedex-battler/static/js/1.0b660870.chunk.js"
+  },
+  {
+    "revision": "134dccf79a09ac27a154",
+    "url": "/pokedex-battler/static/js/runtime~main.134dccf7.js"
+  },
+  {
+    "revision": "f737552190b948c7591d19603f7dc039",
+    "url": "/pokedex-battler/static/media/pokeball.f7375521.png"
+  },
+  {
+    "revision": "5e2f0093d8549ff8dea8d7b23eb91d32",
+    "url": "/pokedex-battler/static/media/battleTheme.5e2f0093.mp3"
+  },
+  {
+    "revision": "1552bf9646da5d4caa4e54b2dbeaf72c",
+    "url": "/pokedex-battler/static/media/victory.1552bf96.mp3"
+  },
+  {
+    "revision": "128b63e315fc574fe7a5a74ed81a7802",
+    "url": "/pokedex-battler/static/media/volume.128b63e3.png"
+  },
+  {
+    "revision": "bdda37c9331b1d4ef380",
+    "url": "/pokedex-battler/static/js/main.bdda37c9.chunk.js"
+  },
+  {
+    "revision": "4ab28086d73a13248fddb49ec61c778e",
+    "url": "/pokedex-battler/static/media/bugicon.4ab28086.png"
+  },
+  {
+    "revision": "a2729ea34c98f4fd65b303d369cd9e34",
+    "url": "/pokedex-battler/static/media/dragonicon.a2729ea3.png"
+  },
+  {
+    "revision": "14549cc284a2d122ec4caa481257ccd0",
+    "url": "/pokedex-battler/static/media/fairyicon.14549cc2.png"
+  },
+  {
+    "revision": "0da01c00504e62f5a1b6cf9f40384f8f",
+    "url": "/pokedex-battler/static/media/fireicon.0da01c00.png"
+  },
+  {
+    "revision": "3c9e8835b2929f5d9d746fb23c8e37e6",
+    "url": "/pokedex-battler/static/media/ghosticon.3c9e8835.png"
+  },
+  {
+    "revision": "af25d64785d5d76bb6b5fc995a71d28b",
+    "url": "/pokedex-battler/static/media/iceicon.af25d647.png"
+  },
+  {
+    "revision": "1ce7dca60d04713d01062f5fe8b1855a",
+    "url": "/pokedex-battler/static/media/physicicon.1ce7dca6.png"
+  },
+  {
+    "revision": "0b66087019812d1c3da3",
+    "url": "/pokedex-battler/static/css/1.06dfd132.chunk.css"
+  },
+  {
+    "revision": "77cf3a03d0542e326c1aedc978776d26",
+    "url": "/pokedex-battler/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/pokedex-battler/precache-manifest.432c7e3e1fae6517ad3fb7ca5c5e1a7d.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/pokedex-battler/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
